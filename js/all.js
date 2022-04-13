21f1eab9fcde3d40d74a055b4d5369d973af0010
Pickachu Chen <pickachu1006@gmail.com>
2022-04-13 11:04:52 +0800

    20220413_lottery-2

--- js/all.js
+++ js/all.js
@@ -7,6 +7,21 @@ $(document).ready(function () {
         }, 900);
     });
 
+    // 日本的modal
+    $('.modal-open-button').click(function () {
+        $('#modal-wrap').css('position', 'absolute');
+    });
+
+    // 日本的modal自動抓取月份最後一天
+    var nowDate = new Date();
+    var year = nowDate.getFullYear()
+    var month = nowDate.getMonth() + 1;
+    month = month < 10 ? '0' + month : month;
+    var lastDay = new Date(`${year}`, `${month}`, 0);
+    var day = lastDay.getDate();
+    day = day < 10 ? '0' + day : day;
+    $('#lastday').text(`${month}/${day}`);
+
     //活動辦法彈跳視窗
     $(".anniversary_2").hide();
     $('.anniversary_1').click(function (e) {
